47bd404155277ed452ea02f5c53e8f122008fa11
Felix <felixlam0316@gmail.com>
2024-10-31 16:09:39 -0400

    - Added directory checks

--- automations/mysql_deeplink_start.ps1
+++ automations/mysql_deeplink_start.ps1
@@ -1,3 +1,11 @@
 SET-LOCATION ..
-mysql_install_db -c mariadb_local.ini -p admin 
+$directory = "mysql/data"
+
+if (Get-ChildItem -Path $directory) {
+    Write-Host "The directory '$directory' contains files."
+} else {
+    Write-Host "The directory '$directory' is empty."
+    mysql_install_db -c mariadb_local.ini -p admin 
+}
+
 mysqld --defaults-file="mariadb_local.ini" --console
